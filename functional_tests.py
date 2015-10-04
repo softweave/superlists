9dadcc4b31e8b24f161c8fd9b766817200ceab74
Jane Eisenstein <softweave@gmail.com>
2015-10-04 12:01:58 -0400

    Completed chapter 4.

--- functional_tests.py
+++ functional_tests.py
@@ -20,13 +20,13 @@ class NewVisitorTest(unittest.TestCase):
         # check out the page title and header
         # assert 'To-Do' in browser.title
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('hi').text
+        header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
         # see invitation to enter a to-do item
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-            inputbox.get.attribute('placeholder'),
+            inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
 
@@ -39,7 +39,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            'New to-do item did not appear in table'
         )
 
         # text box invites adding another item
