c03cccda0c07cce896547600cf48fa170808b468
Jane Eisenstein <softweave@gmail.com>
2015-10-04 11:33:33 -0400

    Functional test now check for input of to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 __author__ = 'janee'
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -19,15 +20,30 @@ class NewVisitorTest(unittest.TestCase):
         # check out the page title and header
         # assert 'To-Do' in browser.title
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('hi').text
+        self.assertIn('To-Do', header_text)
 
         # see invitation to enter a to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get.attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
-        # type "buy peacock featuers" into a text box
+        # type "buy peacock feathers" into a text box
+        inputbox.send_keys('Buy peacock feathers')
 
         # hit enter, page updates and the page lists here entry as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         # text box invites adding another item
+        self.fail('Finish the test!')
 
         # type use peacock feathers to make fan for dancing
 
